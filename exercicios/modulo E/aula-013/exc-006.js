38c441efcad5326130d92d4fd1ff8ed95bb85745
Andressa Giovanna <146788061+andressagiovanna@users.noreply.github.com>
2023-11-13 22:15:52 -0300

    Update

---
+++ exercicios/modulo E/aula-013/exc-006.js
@@ -0,0 +1,25 @@
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+} while (c <= 2) 
+
+
+/* 
+var c = 1
+while (c <= 4) {
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+}
+*/
+
+
+/*
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+*/
